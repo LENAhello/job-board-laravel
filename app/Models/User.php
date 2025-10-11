cf8f24c143dd08b7b5f69c476e67fcf5b8bdd747
LeNa24Shi <lenashihab033@gmail.com>
2025-10-11 20:52:21 +0200

    Create applications table

--- app/Models/User.php
+++ app/Models/User.php
@@ -51,4 +51,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Job::class);
     }
+
+    public function applications()
+    {
+        return $this->hasMany(Application::class);
+    }
 }
